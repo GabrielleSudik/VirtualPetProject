7bc5a5a9d008fc8831daf955c69d5ac3ac6c1ee3
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2017-09-07 21:06:51 -0400

    some updates

---
+++ VirtualPetProject/Pet.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPetProject
+{
+    class Pet
+    {
+        //fields
+
+        private string breed;
+        private bool isHungry;
+        private bool isVegetarian;
+        private int dangerLevel;
+        private int numberOfTeeth;
+        private string sound;
+
+        //properties
+
+        public string Breed
+        {
+            get { return this.breed; }
+            set { this.breed = value; }
+        }
+
+        public bool IsHungry
+        {
+            get { return this.isHungry; }
+            set { this.isHungry = value; }
+        }
+
+        public bool IsVegetarian
+        {
+            get { return this.isVegetarian; }
+            set { this.isVegetarian = value; }
+        }
+
+        public int DangerLevel
+        {
+            get { return this.dangerLevel; }
+            set { this.dangerLevel = value; }
+        }
+
+        public int NumberOfTeeth
+        {
+            get { return this.numberOfTeeth; }
+            set { this.numberOfTeeth = value; }
+        }
+
+        public string Sound
+        {
+            get { return this.sound; }
+            set { this.sound = value; }
+        }
+
+
+        //constructors
+
+        Pet()
+        {
+            this.breed = breed;
+            this.isHungry = isHungry;
+            this.isVegetarian = isVegetarian;
+            this.numberOfTeeth = numberOfTeeth;
+            this.dangerLevel = dangerLevel;
+            this.sound = sound;
+        }
+
+
+        //methods
+    
+        //intro. 
+        //hungry, danger, teeth all in one
+        //or hungry and veggie, danger and teeth
+        //sound, maybe when pet or something
+    }
+}
--- VirtualPetProject/Program.cs
+++ VirtualPetProject/Program.cs
@@ -10,6 +10,32 @@ namespace VirtualPetProject
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Here is your new pet.");
+            Console.WriteLine("First things first, let's name it.");
+            Console.WriteLine("What do you want to call your new pet?");
+            string name = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("OK, now what would you like to do with your pet? Choose from:");
+            Console.WriteLine("   1.  Learn about your pet");
+            Console.WriteLine("   1.  Feed your pet");
+            Console.WriteLine("   2.  Walk your pet");
+            Console.WriteLine("   4.  Play with your pet");
+            Console.WriteLine("   5.  Send your pet to the farm.");
+            int choice = int.Parse(Console.ReadLine());
+
+
+        
+
+
+        }
+
+        static void ToTheFarm()
+        {
+            Console.WriteLine("\nYou do know that \"sending a pet to the farm\" means putting them to sleep, right?");
+            Console.WriteLine("You are a terrible person.");
+            Console.WriteLine("Your pet is now dead.");
+            return;
         }
     }
 }
