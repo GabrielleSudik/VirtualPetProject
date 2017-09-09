fef8c84c563932b579189281cc392a1449d951fb
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2017-09-09 19:49:25 -0400

    basics done, needs tweaking

--- VirtualPetProject/Pet.cs
+++ VirtualPetProject/Pet.cs
@@ -14,7 +14,8 @@ namespace VirtualPetProject
         private bool isHungry;
         private bool isVegetarian;
         private int dangerLevel;
-        private int numberOfTeeth;
+        private int maxDanger; //update this below
+        private int happiness; //add this below
         private string sound;
 
         //properties
@@ -43,10 +44,16 @@ namespace VirtualPetProject
             set { this.dangerLevel = value; }
         }
 
-        public int NumberOfTeeth
+        //public int NumberOfTeeth
+        //{
+        //    get { return this.numberOfTeeth; }
+        //    set { this.numberOfTeeth = value; }
+        //}
+
+            public int Happiness
         {
-            get { return this.numberOfTeeth; }
-            set { this.numberOfTeeth = value; }
+            get { return this.happiness; }
+            set { this.happiness = value; }
         }
 
         public string Sound
@@ -58,22 +65,168 @@ namespace VirtualPetProject
 
         //constructors
 
-        Pet()
+        public Pet()
         {
-            this.breed = breed;
+            this.breed = "Tricerotops";
+            this.isHungry = !isHungry;
+            this.isVegetarian = true;
+            //this.numberOfTeeth = numberOfTeeth;
+            this.dangerLevel = 6;
+            this.happiness = 0;
+            this.sound = "** subsonic rumble **";  //update these
+        }
+
+        public Pet(string breed)  //can you use this to assign stats to one type, and the default to the other type?
+        {
+            this.breed = "Tyrannasaurus Rex";
             this.isHungry = isHungry;
-            this.isVegetarian = isVegetarian;
-            this.numberOfTeeth = numberOfTeeth;
-            this.dangerLevel = dangerLevel;
-            this.sound = sound;
+            this.isVegetarian = !isVegetarian;
+            //this.numberOfTeeth = 60;
+            this.dangerLevel = 10;
+            this.sound = "RRRRRRAAAAAAAARRRRRRRGGGGGHHHHHHHHHHHH!!!!!";
         }
 
 
+
         //methods
-    
+
         //intro. 
+        //a show stats method. maybe accept the name here.
         //hungry, danger, teeth all in one
         //or hungry and veggie, danger and teeth
         //sound, maybe when pet or something
+
+    public void AboutPet(string breed)
+        {
+            if (breed == "T Rex")
+            {
+                Console.WriteLine("\nThe T Rex is an amazingly hostile beast!");
+                Console.WriteLine("It's really rude and dangerous, so be careful about taking it outside.");
+                Console.WriteLine("But its bloodlust can be sated with a tasty slab of beef.");
+                Console.WriteLine("When cuddled, it will emit an agonizingly loud ROAR.");
+                Console.WriteLine("A very exiting pet.");
+            }
+
+            else
+            {
+                Console.WriteLine("The Tricerotops is slow and large, but relatively tame.");
+                Console.WriteLine("Compared to many dinosaurs, it is only slightly dangerous.");
+                Console.WriteLine("When upset, it should be fed lots of bamboo.");
+                Console.WriteLine("When cuddled, it will hum at a sub-sonic pitch.");
+                Console.WriteLine("A gentle pet (for a dinosaur).");
+            }
+        }
+
+
+        public void FeedPet(string breed)
+        {
+            Console.WriteLine("\nDo you want to give your pet:");
+            Console.WriteLine("  beef or bamboo?");
+            string food = Console.ReadLine();
+
+            if (breed == "Tricerotops")
+            {
+                if (food == "bamboo")
+                {
+                    //consider a pet happiness increase?
+                    dangerLevel -= 4;
+                    Console.WriteLine("Your pet is happier and less dangerous.");
+                }
+
+                else
+                {
+                    dangerLevel += 2;
+                    Console.WriteLine("Your pet is upset at the thought of eating flesh.");
+                    Console.WriteLine(  "It is now angry and more dangerous!");
+                }
+            }
+
+            else
+            {
+                if (food == "beef")
+                {
+                    //consider a pet happiness increase?
+                    dangerLevel -= 4;
+                    Console.WriteLine("Your pet is full and less dangerous.");
+                }
+
+                else
+                {
+                    dangerLevel += 2;
+                    Console.WriteLine("\nYour pet hates salad! It is now angry and more dangerous!");
+                }
+            }
+            
+        }
+
+        public void WalkPet()
+        {
+            if (dangerLevel <= 3)
+            {
+                Console.WriteLine("\nYour pet is hungry, and too dangerous to take outside.");
+                Console.WriteLine("Hit return to see the menu again (hint: Feed your pet).");
+            }
+            //    string answer = Console.ReadLine();
+
+            //    if (answer == "yes")
+            //    {
+            //        return; //not sure if return is the right word??
+            //    }
+
+            //    else 
+            //            {
+            //        Console.WriteLine("A hungry dinosaur is a dangerous dinosaur.");
+            //        Console.WriteLine("You've been eaten by your own pet.");
+            //        Console.WriteLine("Next time, just feed it, ok?");
+
+            //        return;
+            //    }
+            //}
+
+            else
+            {
+                Console.WriteLine("\nYour pet is satisfied and calm. You walk it without incident.");
+            }
+        }
+
+        public void PetPet()
+        {
+            Console.WriteLine("\nWhich do you want to do to play with your pet:");
+            Console.WriteLine("   cuddle, chase or tease?");
+            string playChoice = Console.ReadLine();
+
+            if (playChoice == "cuddle")
+            {
+                happiness += 3;
+                Console.WriteLine("Your pet loves you and is much happier!");
+            }
+
+            else if (playChoice == "chase")
+            {
+                happiness += 2;
+                Console.WriteLine("Your pet got some exercise and is somewhat happier.");
+            }
+
+            else 
+                    {
+                happiness--;
+                Console.WriteLine("Your pet is less happy.");
+            }
+
+            if (happiness >= 3)
+            {
+                Console.WriteLine($"Your pet lets out a joyful {sound}.");
+            }
+
+            else if (happiness >= 0 && happiness <= 2)
+            {
+                Console.WriteLine("Your pet could use some more play time.");
+            }
+
+            else
+            {
+                Console.WriteLine("Your pet is angry. Better play with it a lot more!");
+            }
+        }
     }
 }
--- VirtualPetProject/Program.cs
+++ VirtualPetProject/Program.cs
@@ -10,32 +10,94 @@ namespace VirtualPetProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Here is your new pet.");
-            Console.WriteLine("First things first, let's name it.");
-            Console.WriteLine("What do you want to call your new pet?");
+            Console.WriteLine("First things first...");
+            Console.WriteLine("Would you like to adopt a \"T Rex\" or a \"Tricerotops\"?");
+            string breed = Console.ReadLine();
+
+            Console.WriteLine($"\nCongrats! You now own a beautiful {breed}!"); //maybe offer choice, trex or steg
+                                                                                //then if else depending on choice, with different constructors
+                                                                                //one veggie, one beef, etc.
+
+            Console.WriteLine("What would you like to name it?");
             string name = Console.ReadLine();
             Console.WriteLine();
 
-            Console.WriteLine("OK, now what would you like to do with your pet? Choose from:");
-            Console.WriteLine("   1.  Learn about your pet");
-            Console.WriteLine("   1.  Feed your pet");
-            Console.WriteLine("   2.  Walk your pet");
-            Console.WriteLine("   4.  Play with your pet");
-            Console.WriteLine("   5.  Send your pet to the farm.");
-            int choice = int.Parse(Console.ReadLine());
+            Pet dino = new Pet();
+
+            //if (breed == "Tricerotops")
+            //{
+            //    Pet dino = new Pet();
+            //}
+
+            bool isAlive = true;
+
+            //make this a loop somehow
+
+            while (isAlive == true)
+            {
+                Console.WriteLine($"OK, now what would you like to do with {name}? Choose from:");
+                Console.WriteLine("   1.  Learn about your pet");
+                Console.WriteLine("   2.  Feed your pet");
+                Console.WriteLine("   3.  Walk your pet");
+                Console.WriteLine("   4.  Play with your pet");
+                Console.WriteLine("   5.  Send your pet to the farm.");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                if (choice == 1)
+
+                { dino.AboutPet(breed); }
+
+                //{
+                //    if (breed == "T Rex")
+                //    {
 
 
-        
+                //        dino.AboutPet(breed);
+                //    }
+
+                //    if (breed == "Tricerotops")
+                //    {
+                //        dino.AboutPet();
+                //    }
+                //}
+
+                if (choice == 2)
+                {
+                    dino.FeedPet(breed);
+                }
+
+                if (choice == 3)
+                {
+                    dino.WalkPet();
+                }
+
+                if (choice == 4)
+                {
+                    dino.PetPet();
+                }
+
+                if (choice == 5)
+                {
+                    ToTheFarm(name);
+                    isAlive = false;
+                }
+
+                Console.ReadLine();
 
 
+            }
         }
 
-        static void ToTheFarm()
+        static void ToTheFarm(string name)
         {
-            Console.WriteLine("\nYou do know that \"sending a pet to the farm\" means putting them to sleep, right?");
+            Console.WriteLine("\nYou \"send your pet to the farm.\"");
+            Console.WriteLine($"{name} is now dead.");
             Console.WriteLine("You are a terrible person.");
-            Console.WriteLine("Your pet is now dead.");
-            return;
+
+            
+
+            //return; //maybe instead of return here... the loop will end above and you can just close out?
         }
     }
 }
